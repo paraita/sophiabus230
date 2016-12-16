2fce0f1e82ea3cd5915030effd31f3f1306d1aea
Paraita Wohler <paraita.wohler@activeeon.com>
2016-12-16 14:16:38 +0100

    First support of package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='sophiabus230',
+      version='0.1',
+      description='Module to get the timetable of the Sophia Antipolis bus line 230',
+      url='http://github.com/paraita/sophiabus230',
+      author='Paraita Wohler',
+      author_email='paraita.wohler@gmail.com',
+      license='',
+      packages=[],
+      zip_safe=False)
