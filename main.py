bdf08268401d9963c128b518d9c34fd60ee9b9d1
Paraita Wohler <paraita.wohler@activeeon.com>
2016-12-16 13:08:52 +0100

    Modify the return format

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import sophiabus230
 
 for t in sophiabus230.get_next_buses():
-    print t
-
-
-
+    bus_time = t['bus_time']
+    dest = t['dest']
+    print "Next bus at {0} to {1}".format(bus_time, dest)
--- sophiabus230/__init__.py
+++ sophiabus230/__init__.py
@@ -3,25 +3,11 @@
 
 import urllib
 import re
+from datetime import datetime
+from dateutil.parser import parse
+from dateutil.tz import gettz
+from datetime import timedelta
 from bs4 import BeautifulSoup
-
-
-def _translate_to_eng(french_str):
-    """
-    Utilitary to replace words from a french sentence with english words.
-    Replaces the following:
-    - `a` to `at`
-    - `dans` to `in`
-    - `direction` to `towards`
-
-    :param french_str: the french sentence to translate
-    :return: the translated sentence
-    """
-    translated = french_str.replace(u'à', 'at')
-    translated = translated.replace(u'é', 'e')
-    translated = translated.replace('dans', 'in')
-    translated = translated.replace('direction', 'towards')
-    return translated
 
 
 def _get_html_from_cg06(stop_id):
@@ -38,6 +24,57 @@ def _get_html_from_cg06(stop_id):
     return content
 
 
+def _sanitize_entry(entry):
+    """
+    Sanitize the entry by removing the extra spaces and empty lines
+
+    :param entry: the entry to sanitize
+    :return: the entry without unecessary spaces
+    :rtype: str
+    """
+    sane_line = re.sub(r"^\s+$", '\n', entry)
+    sane_line.replace(u"é", "e")
+    if len(sane_line) > 1:
+        return sane_line
+    else:
+        return None
+
+
+def _parse_entry(entry):
+    """
+    Convert a entry into a dict containing the following attributes:
+
+    - bus_time
+    - dest
+    - is_real_time
+
+    :param entry: the string representation of one bus passage
+    :return: a dict representing the relevant information of the string
+    :rtype: dict
+    """
+    split_entry = entry.split(' ')
+    idx_end_direction = -1
+    is_real_time = True
+    tz_paris = gettz('Europe/Paris')
+    if split_entry[-1] == '*':
+        is_real_time = False
+        idx_end_direction = len(split_entry) - 1
+    if split_entry[0] == 'dans':
+        # case for immediate upcoming bus
+        date_now = datetime.now(tz=tz_paris)
+        delta_minutes = int(split_entry[1])
+        bus_time = date_now + timedelta(minutes=delta_minutes)
+        idx_start_direction = 4
+    else:
+        # case for later upcoming bus
+        tzinfos = {'FR': tz_paris}
+        bus_time = parse(split_entry[1], tzinfos=tzinfos)
+        idx_start_direction = 3
+    dest = ' '.join(split_entry[idx_start_direction:idx_end_direction])
+    dest = dest.encode('utf-8')
+    return {'bus_time': bus_time, 'dest': dest, 'is_real_time': is_real_time}
+
+
 def get_next_buses():
     """
     Fetches the list of upcoming buses at the INRIA bus stop for the bus 230 towards Nice
@@ -52,11 +89,9 @@ def get_next_buses():
     data = soup.find_all("div", attrs={"class": "data"})
     tt = []
     for e in data[0].div.get_text().split('\n'):
-        sane_line = re.sub(r"^\s+$", '\n', e)
-        sane_line = _translate_to_eng(sane_line)
-        if len(sane_line) > 1:
-            tt.append(sane_line)
+        sane_entry = _sanitize_entry(e)
+        if sane_entry is not None:
+            print sane_entry
+            tt.append(_parse_entry(sane_entry))
     return tt
-
-
 
