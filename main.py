15e7d8ea65b9a82f528a927e85f0740fe340c85a
Paraita Wohler <paraita.wohler@activeeon.com>
2016-12-16 14:16:14 +0100

    Fix the test to show better debuging info

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 import sophiabus230
 
-for t in sophiabus230.get_next_buses():
+for t in sophiabus230.get_next_buses(debug=True):
     bus_time = t['bus_time']
     dest = t['dest']
-    print "Next bus at {0} to {1}".format(bus_time, dest)
+    rt = "theoretical"
+    if t['is_real_time']:
+        rt = "real time"
+    print "Next bus at {0} to {1} ({2})".format(bus_time, dest, rt)
